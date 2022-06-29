3377b39cb9d87420c0969e4a9c8a20c2b6edf38e
dubulahla2 <100410536+dubulahla2@users.noreply.github.com>
2022-06-29 12:21:49 -0700

    iddle

---
+++ transportFee.js
@@ -0,0 +1,14 @@
+// function transportFee(rightPrice){
+// switch(rightPrice){
+//     case "morning":
+//       return "R20"
+//       break;
+//     case "afternoon":
+//       return "R10"
+//       break;
+//     default:
+//       return "free";
+//                    }
+//   }
+ 
+  
